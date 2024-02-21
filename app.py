376dcbae14442af3f1b8804f725d4e2e4b229c6a
Amrutha A <amruthaammu2012005@gmail.com>
2024-02-21 10:32:26 +0530

    save changes

--- app.py
+++ app.py
@@ -11,7 +11,10 @@ generation_config = {  "temperature": 0.4,  "top_p": 1,  "top_k": 32,
 }
 
 safety_settings = [{"category": "HARM_CATEGORY_HARASSMENT",
-"threshold": "BLOCK_MEDIUM_AND_ABOVE" },{"category": "HARM_CATEGORY_HATE_SPEECH",  "threshold": "BLOCK_MEDIUM_AND_ABOVE"},{"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",  "threshold": "BLOCK_MEDIUM_AND_ABOVE"  },  {   "category": "HARM_CATEGORY_DANGEROUS_CONTENT",   "threshold": "BLOCK_MEDIUM_AND_ABOVE" },
+"threshold": "BLOCK_MEDIUM_AND_ABOVE" },
+{"category": "HARM_CATEGORY_HATE_SPEECH",  "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+{"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",  "threshold": "BLOCK_MEDIUM_AND_ABOVE"  }, 
+{   "category": "HARM_CATEGORY_DANGEROUS_CONTENT",   "threshold": "BLOCK_MEDIUM_AND_ABOVE" },
 ]
 
 system_prompt=""" 
